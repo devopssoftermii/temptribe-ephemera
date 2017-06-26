970dd364911eb30214f8b3558abab0fa0c0ad732
Tommy Crosby <tcrosby@gmail.com>
2017-06-26 17:32:42 +0100

    Cleaner logging middleware

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var logging = require('./middleware/logging'),
-    app     = require('express')();
-
 // Import config into process.env
 require('dotenv-safe').config();
 
-// Logging
-app.use(logging.before);
+// Import Express and middleware
+var logging = require('./middleware/logging'),
+    data    = require('./data'),
+    app     = require('express')();
+
+// Pre-request logging
+logging.before.forEach(function(middleware) {
+  app.use(middleware);
+});
 
 // JSON parser
 app.use(require('body-parser').json());
@@ -13,11 +17,19 @@ app.use(require('body-parser').json());
 // App
 app.use('/api/v2', require('./app'));
 
-// Error handling
-app.use(logging.after);
+// Post-request error handling
+logging.after.forEach(function(middleware) {
+  app.use(middleware);
+});
 
+// Create HTTP server and listen
 const port = process.env.PORT || 3002;
 const host = process.env.HOST || 'localhost';
 require('http').createServer(app).listen(port, host, function (err) {
-  console.log('Listening for requests on http://' + host + ':' + port);
+  console.log(`
+>>  Listening for requests on http://${host}:${port}
+>>  Logging access requests locally, exceptions to Sentry if configured
+
+Ready.
+  `);
 });
--- middleware/logging.js
+++ middleware/logging.js
@@ -3,36 +3,35 @@ var morgan = require('morgan'),
     path 			= require('path'),
     fs 				= require('fs');
 
+module.exports = {
+  before: [],
+  after: []
+};
+
 // Sentry logging
-var Raven = null;
+var ravenLogger = null;
+var errorLogger = null;
 if (process.env.SENTRY_DSN) {
   var Raven = require('raven');
   Raven.config(process.env.SENTRY_DSN).install();
   Raven.captureMessage('Initialising Ephemera API', {
     level: 'info'
   });
+  module.exports.before.push(Raven.requestHandler());
+  module.exports.after.push(Raven.errorHandler());
 }
 
 // HTTP access logging
 var logDir = process.env.LOGDIR || 'log';
 fs.existsSync(logDir) || fs.mkdirSync(logDir);
-var accessLogger = morgan('combined', {
-  stream: rfs('access.log', {
-    interval: '1d',
-    path: logDir
-  })
-})
 
-module.exports = {
-  before: function(req, res, next) {
-    if (Raven) {
-      Raven.requestHandler()(req, res, next);
-    }
-    accessLogger(req, res, next);
-  },
-  after: function(req, res, next) {
-    if (Raven) {
-      Raven.errorHandler()(req, res, next);
-    }
-  }
-};
+module.exports.before.push(
+  morgan('combined', {
+    stream: rfs('access.log', {
+      interval: '1d',
+      path: logDir
+    })
+  })
+);
+
+module.exports.before.push(morgan('dev'));
