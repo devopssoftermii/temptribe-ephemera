8ec6f617906e10c3689012892e5cf90aab0a0920
Tommy Crosby <tcrosby@gmail.com>
2017-06-27 13:41:10 +0100

    Added generic route resolution and initial event/user routes

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,5 +1,9 @@
 var jwt = require('express-jwt');
 
 module.exports = function(req, res, next) {
-  jwt({ secret: process.env.JWT_SECRET })(req, res, next);
+  if (process.env.SKIP_LOGIN === 'true') {
+    next();
+  } else {
+    jwt({ secret: process.env.JWT_SECRET })(req, res, next);
+  }
 };
--- app/routes/private/index.js
+++ app/routes/private/index.js
@@ -1,7 +1,16 @@
-var router  = require('express').Router();
+var router    = require('express').Router(),
+    path      = require('path'),
+    fs        = require('fs');
+
+fs.readdirSync(__dirname).forEach(function(filename) {
+  var route = path.basename(filename, '.js');
+  if (route !== path.basename(__filename, '.js')) {
+    router.use('/' + route, require(`${__dirname}/${route}`));
+  }
+});
 
 router.use('/', function(req, res, next) {
-  res.send('Private endpoint');
+  res.status(404).end();
 });
 
 module.exports = router;
