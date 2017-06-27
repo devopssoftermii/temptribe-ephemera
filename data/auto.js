e7daa5c7ef0ac4ab46bd80e3481018fc9f272dd0
Tommy Crosby <tcrosby@gmail.com>
2017-06-27 13:13:58 +0100

    Actually do the thing, plus use config

--- data/auto.js
+++ data/auto.js
@@ -1,6 +1,9 @@
-var auto = new SequelizeAuto('LolaLocal', 'sequelize_test', 'abcdefgh123', {
-    host: 'LOLA02',
-    dialect: 'mssql',
+require('dotenv-safe').config({ path: '../.env' });
+var SequelizeAuto = require('sequelize-auto');
+
+var auto = new SequelizeAuto(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     directory: `${__dirname}/models`, // prevents the program from writing to disk
     additional: {
         timestamps: false,
