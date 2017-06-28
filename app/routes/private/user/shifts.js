ed296d6a20bdbe8241e27343cec748e7d2ca7caa
Tommy Crosby <tcrosby@gmail.com>
2017-06-28 13:13:40 +0100

    Include other event details

--- app/routes/private/user/shifts.js
+++ app/routes/private/user/shifts.js
@@ -2,7 +2,9 @@ module.exports = function(req, res, next) {
   var sequelize = req.app.locals.sequelize;
   var models = req.app.locals.models;
   models.users.findById(226890, {
+    attributes: [],
     include: [{
+      attributes: [],
       model: models.userTimesheets,
       as: 'timesheets',
       where: {
@@ -10,22 +12,74 @@ module.exports = function(req, res, next) {
       },
       include: [{
         model: models.eventShifts,
+        attributes: [
+          'id',
+          [sequelize.fn('convert', sequelize.literal('VARCHAR(5)'), sequelize.col('originalStartTime'), 108), 'startTime'],
+          [sequelize.fn('convert', sequelize.literal('VARCHAR(5)'), sequelize.col('originalFinishTime'), 108), 'endTime'],
+          'originalStartTime',
+          'originalFinishTime',
+          'hourlyRate',
+        ],
         as: 'shift',
         required: true,
         include: [{
           model: models.events,
+          attributes: [
+            'id',
+            'comments',
+            'title',
+            'subtitle'
+          ],
           as: 'event',
           where: {
             eventDate: {
               $gt: sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))
             }
-          }
+          },
+          include: [{
+            model: models.venues,
+            attributes: [
+              'name',
+              'address1',
+              'address2',
+              'town',
+              'county',
+              'postcode',
+              'mapLink',
+              [sequelize.fn(
+                'concat',
+                '/images/venuePhotos/',
+                sequelize.fn(
+                  'convert',
+                  sequelize.literal(varchar(10)),
+                  sequelize.col('id')
+                ),
+                '.jpg'
+              ), 'imageURL']
+            ],
+            as: 'venue'
+          }, {
+            model: models.clients,
+            attributes: [['clientName', 'name']],
+            as: 'client'
+          }]
+        }, {
+          model: models.dressCodes,
+          attributes: [
+            ['ShortDescription', 'shortDescription'],
+            'description'
+          ],
+          as: 'dressCode'
+        }, {
+          model: models.jobRoles,
+          attributes: [
+            'title'
+          ],
+          as: 'jobRole'
         }]
       }]
     }]
   }).then(function(result) {
-    res.json(result.timesheets.map(function(timesheet) {
-      return timesheet.shift;
-    }));
+    res.json(result.timesheets);
   });
 }
