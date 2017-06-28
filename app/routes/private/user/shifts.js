9b006aab70bc95505167802e4663b62c29265dba
Tommy Crosby <tcrosby@gmail.com>
2017-06-28 13:22:22 +0100

    Revert "Unambiguify cols"
    This reverts commit 8910100d776c0e03cff2266c3259a502f12864b7.

--- app/routes/private/user/shifts.js
+++ app/routes/private/user/shifts.js
@@ -13,7 +13,7 @@ module.exports = function(req, res, next) {
       include: [{
         model: models.eventShifts,
         attributes: [
-          sequelize.col('id'),
+          'id',
           [sequelize.fn('convert', sequelize.literal('VARCHAR(5)'), sequelize.col('originalStartTime'), 108), 'startTime'],
           [sequelize.fn('convert', sequelize.literal('VARCHAR(5)'), sequelize.col('originalFinishTime'), 108), 'endTime'],
           'originalStartTime',
@@ -25,10 +25,10 @@ module.exports = function(req, res, next) {
         include: [{
           model: models.events,
           attributes: [
-            sequelize.col('id'),
-            sequelize.col('comments'),
-            sequelize.col('title'),
-            sequelize.col('subtitle')
+            'id',
+            'comments',
+            'title',
+            'subtitle'
           ],
           as: 'event',
           where: {
@@ -39,7 +39,7 @@ module.exports = function(req, res, next) {
           include: [{
             model: models.venues,
             attributes: [
-              sequelize.col('name'),
+              'name',
               'address1',
               'address2',
               'town',
@@ -60,20 +60,20 @@ module.exports = function(req, res, next) {
             as: 'venue'
           }, {
             model: models.clients,
-            attributes: ['clientName'],
+            attributes: [['clientName', 'name']],
             as: 'client'
           }]
         }, {
           model: models.dressCodes,
           attributes: [
             ['ShortDescription', 'shortDescription'],
-            sequelize.col('description')
+            'description'
           ],
           as: 'dressCode'
         }, {
           model: models.jobRoles,
           attributes: [
-            sequelize.col('title')
+            'title'
           ],
           as: 'jobRole'
         }]
