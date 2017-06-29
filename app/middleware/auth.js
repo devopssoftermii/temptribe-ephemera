8759639f07453d3f4ecbab112651cc9bd1610a2b
Tommy Crosby <tcrosby@gmail.com>
2017-06-29 12:55:34 +0100

    Better handling of JWT bailing

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,16 +1,22 @@
-var jwt = require('express-jwt');
-
-module.exports = function(req, res, next) {
+module.exports = function(router) {
   if (process.env.SKIP_LOGIN === 'true') {
-    next();
-  } else {
-    try {
-      jwt({ secret: process.env.JWT_SECRET })(req, res, next);
-    } catch (ex) {
-      res.status(401).json({
-        authorised: false,
-        message: ex.message
-      });
-    }
+    return;
   }
-};
+  router.use(require('express-jwt')({ secret: process.env.JWT_SECRET }));
+  router.use(function(err, req, res, next) {
+    res.status(401).json({
+      error: true,
+      message: err.message
+    });
+  });
+  router.use(function(req, res, next) {
+    if (!req.user || !req.user.id) {
+      res.status(500).json({
+        error: true,
+        message: 'Authenticated but no user set - something has gone wrong'
+      });
+    } else {
+      next();
+    }
+  });
+}
--- app/routes/private/index.js
+++ app/routes/private/index.js
@@ -2,17 +2,7 @@ var router    = require('express').Router(),
     path      = require('path'),
     fs        = require('fs');
 
-router.use(require('../../middleware/auth'));
-router.use(function(req, res, next) {
-  if (!req.user || !req.user.id) {
-    res.status(500).json({
-      error: true,
-      message: 'Authenticated but no user set - something has gone wrong'
-    });
-  } else {
-    next();
-  }
-});
+require('../../middleware/auth')(router);
 
 fs.readdirSync(__dirname).forEach(function(filename) {
   var route = path.basename(filename, '.js');
