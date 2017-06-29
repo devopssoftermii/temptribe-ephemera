87daf68555306200444a526573643dab0cd23fee
Tommy Crosby <tcrosby@gmail.com>
2017-06-29 16:45:34 +0100

    test promise bail on session

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -2,15 +2,13 @@ var jwt = require('jsonwebtoken');
 
 module.exports = {
   create: function(user, models) {
-    return models.apiSession.create({ userId: user.id }).then(function(session) {
+    return models.apiSession.create({ userId: user }).then(function(session) {
       return {
         success: true,
         userId: user.id,
         access: jwt.sign(user.get({ plain: true }), process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_TTL, 10) }),
         refresh: jwt.sign(session.get({ plain: true }), process.env.JWT_SECRET, { expiresIn: 86400 * 365 })
       }
-    }).catch(function(err) {
-      throw err;
     });
   }
 };
