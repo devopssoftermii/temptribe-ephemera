7faf8b40ee634c2a14c38b2b0cb7052c18d0f926
Tommy Crosby <tcrosby@gmail.com>
2017-06-29 17:31:18 +0100

    rename param

--- app/routes/public/session/refresh.js
+++ app/routes/public/session/refresh.js
@@ -2,14 +2,14 @@ var session = require('../../../middleware/session');
 
 module.exports = function(router) {
   router.post('/refresh', function(req, res, next) {
-    if (!req.body.refresh) {
+    if (!req.body.token) {
       res.status(401).json({
         success: false,
         error: 'Missing refresh token'
       });
       return;
     }
-    return session.refresh(req.body.refresh, req.app.locals.models).then(function(refreshResult) {
+    return session.refresh(req.body.token, req.app.locals.models).then(function(refreshResult) {
       res.json(refreshResult);
     }).catch(function(err) {
       res.status(401).json({
