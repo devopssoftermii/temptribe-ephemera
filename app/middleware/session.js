27a618d1f97bb9e39bbeb46937d3ac2969ac9e56
Tommy Crosby <tcrosby@gmail.com>
2017-06-29 16:38:40 +0100

    save user under the correct field

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -2,16 +2,15 @@ var jwt = require('jsonwebtoken');
 
 module.exports = {
   create: function(result, models) {
-    var user = result.get({ plain: true });
-    return models.apiSession.create({ user_id: result }).then(function(session) {
+    return models.apiSession.create({ userId: result }).then(function(session) {
       return {
         success: true,
-        userID: user.id,
-        access: jwt.sign(user, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_TTL, 10) }),
-        refresh: jwt.sign(session, process.env.JWT_SECRET, { expiresIn: 86400 * 365 })
+        userId: user.id,
+        access: jwt.sign(result.get({ plain: true }), process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_TTL, 10) }),
+        refresh: jwt.sign(session.get({ plain: true }), process.env.JWT_SECRET, { expiresIn: 86400 * 365 })
       }
     }).catch(function(err) {
-      console.log(err.message);
+      throw err;
     });
   }
 };
