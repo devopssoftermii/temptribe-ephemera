28ecadfce3f120d0de5286d86cf3ea58e2fd8868
Tommy Crosby <tcrosby@gmail.com>
2017-06-29 16:32:19 +0100

    attempt to create session

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -1,16 +1,17 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = {
-  create: function(result) {
+  create: function(result, models) {
     var user = result.get({ plain: true });
-    console.log(result);
-    return app.locals.models.apiSession.create({ user_id: result }).then(function(session) {
+    return models.apiSession.create({ user_id: result }).then(function(session) {
       return {
         success: true,
         userID: user.id,
         access: jwt.sign(user, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_TTL, 10) }),
         refresh: jwt.sign(session, process.env.JWT_SECRET, { expiresIn: 86400 * 365 })
       }
+    }).catch(function(err) {
+      console.log(err.message);
     });
   }
 };
