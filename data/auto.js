1f58dd9d48a3ff43acc8921c654eff9b3f76ae86
Tommy Crosby <tcrosby@gmail.com>
2017-07-06 13:01:08 +0100

    Update autogen list

--- data/auto.js
+++ data/auto.js
@@ -21,6 +21,7 @@ var auto = new SequelizeAuto(process.env.DB_DATABASE, process.env.DB_USERNAME, p
       'clients',
       'events',
       'eventShifts',
+      'eventShiftSuitabilityTypes',
       'venues',
 			'dressCodes',
 			'jobRoles',
