f5e78103f5170425a8e9370234913b3d31e69553
Tommy Crosby <tcrosby@gmail.com>
2017-07-06 14:52:12 +0100

    filter by chosen types

--- data/filters/suitabilityTypes.js
+++ data/filters/suitabilityTypes.js
@@ -1,8 +1,14 @@
 module.exports = function(req, models, output) {
   var typeList = req.user.suitabilityTypes.map(function(type) {
     return type.id;
-  }).sort();
-  output.key.suitabilityTypes = typeList;
+  });
+  if (req.query.filters && Array.isArray(req.query.filters.suit)) {
+    var selectedSet = new Set(req.query.filters.suit);
+    typeList = Array.from(new Set(typeList)).filter(function(item) {
+      return selectedSet.has(item);
+    });
+  }
+  output.key.suitabilityTypes = typeList.sort();
   if (!output.scope.include) {
     output.scope.include = [];
   }
