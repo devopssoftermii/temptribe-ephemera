6d861d3c3006783a74805829ed3118b65a3a26a4
Ash <ashleigh@temptribe.co.uk>
2017-07-06 15:58:21 +0100

    Fix filter

--- data/filters/suitabilityTypes.js
+++ data/filters/suitabilityTypes.js
@@ -2,23 +2,17 @@ module.exports = function(req, models, output) {
   var typeList = req.user.suitabilityTypes.map(function(type) {
     return type.id;
   });
-  var selectedSet;
   if (req.query.filters && req.query.filters.suit) {
-      if (Array.isArray(req.query.filters.suit)) {
-          selectedSet = new Set(req.query.filters.suit);
-      } else {
-          var testType = parseInt(req.query.filters.suit, 10);
-          if (!isNaN(testType)) {
-              selectedSet = new Set([testType]);
-          }
+      var filterSuit = req.query.filters.suit;
+      if (!Array.isArray(filterSuit)) {
+          filterSuit = [filterSuit];
       }
-      if (selectedSet) {
-          var testList = Array.from(new Set(typeList)).filter(function(item) {
-              return selectedSet.has(item.toString());
-          });
-          if (testList.length) {
-              typeList = testList;
-          }
+      var selectedSet = new Set(filterSuit);
+      var testList = Array.from(new Set(typeList)).filter(function(item) {
+          return selectedSet.has(item.toString());
+      });
+      if (testList.length) {
+          typeList = testList;
       }
   }
   output.key.suitabilityTypes = typeList.sort();
