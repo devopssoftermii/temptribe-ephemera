454eb824e9ff4d154f15631989f7d5b388e54a3d
Ash <ashleigh@temptribe.co.uk>
2017-07-12 17:38:41 +0100

    move error handling

--- index.js
+++ index.js
@@ -37,30 +37,7 @@ app.use(apiPath, require('./app'));
 
 // Post-request error handling
 logging.after(app);
-
-app.use(function(err, req, res, next) {
-  var status = err.status || err.status_code || 500;
-  var stack = err.stack || null;
-  var name = err.name || 'ServerError';
-  var code = err.code || 'internal_error';
-  if (process.env.NODE_ENV !== 'development') {
-    res.status(status).json({
-      error: true,
-      message: status === 500? 'Internal server error': err.message,
-      code
-    }).end();
-  } else if (err instanceof Error) {
-    res.status(status).json({
-      error: true,
-      name,
-      message: err.message,
-      code,
-      stack
-    }).end();
-  } else {
-    res.status(status).json(err).end();
-  }
-});
+require('./middleware/errors')(app);
 
 // Create HTTP server and listen
 const port = process.env.PORT || 3002;
---
+++ middleware/errors.js
@@ -0,0 +1,25 @@
+module.exports = function(app) {
+  app.use(function(err, req, res, next) {
+    var status = err.status || err.status_code || 500;
+    var stack = err.stack || null;
+    var name = err.name || 'ServerError';
+    var code = err.code || 'internal_error';
+    if (process.env.NODE_ENV !== 'development') {
+      res.status(status).json({
+      error: true,
+      message: status === 500? 'Internal server error': err.message,
+      code
+      }).end();
+    } else if (err instanceof Error) {
+      res.status(status).json({
+      error: true,
+      name,
+      message: err.message,
+      code,
+      stack
+      }).end();
+    } else {
+      res.status(status).json(err).end();
+    }
+  });
+}
