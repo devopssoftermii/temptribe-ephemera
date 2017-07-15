e71902217b3b9aebdd39bca3626d65246702c5d8
Ashleigh Crosby <tcrosby@gmail.com>
2017-07-15 19:05:49 +0100

    Easier refresh functionality

--- app/middleware/session.js
+++ app/middleware/session.js
@@ -2,6 +2,35 @@ var jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../../lib/errors/UnauthorizedError');
 const ServerError = require('../../lib/errors/ServerError');
 
+function extractSessionFromToken(token, models) {
+  return new Promise(function(resolve, reject) {
+    try {
+      resolve(jwt.verify(token, process.env.JWT_SECRET));
+    } catch (ex) {
+      reject(new UnauthorizedError('invalid_refresh', {message: 'Invalid refresh token'}));
+    }
+  }).then(function(payload) {
+    return models.apiSession.findOne({
+      where: {
+        id: payload.id
+      },
+      include: [{
+        model: models.users,
+        attributes: [
+          'id',
+          'email'
+        ],
+        required: true
+      }]
+    });
+  }).then(function(session) {
+    if (!session) {
+      throw new UnauthorizedError('invalid_session', {message: 'Invalid session'});
+    }
+    return session;
+  })
+}
+
 module.exports = {
   create: function(user, models) {
     return models.apiSession.create({ userId: user.id }).then(function(session) {
@@ -16,42 +45,24 @@ module.exports = {
     });
   },
   destroy: function(token, models) {
-    return new Promise(function(resolve, reject) {
-      try {
-        resolve(jwt.verify(token, process.env.JWT_SECRET));
-      } catch (ex) {
-        reject(new UnauthorizedError('invalid_refresh', {message: 'Invalid refresh token'}));
-      }
-    }).then(function(payload) {
-      return models.apiSession.findOne({
-        where: {
-          id: payload.id
-        },
-        include: [{
-          model: models.users,
-          attributes: [
-            'id',
-            'email'
-          ],
-          required: true
-        }]
-      });
-    }).then(function(result) {
-      if (!result) {
-        throw new UnauthorizedError('invalid_session', {message: 'Invalid session'});
-      }
-      return Promise.all([result.user, result.destroy()]);
-    }).then(function(results) {
-      return results[0];
+    return extractSessionFromToken(token, models).then(function(session) {
+      return session.destroy();
     }).catch(function(err) {
       throw err;
     });
   },
   refresh: function(token, models) {
-    return module.exports.destroy(token, models).then(function(user) {
-      return module.exports.create(user, models);
+    return extractSessionFromToken(token, models).then(function(session) {
+      var user = session.user.get({ plain: true });
+      return {
+        success: true,
+        userId: user.id,
+        access: jwt.sign(user, process.env.JWT_SECRET, { expiresIn: parseInt(process.env.JWT_TTL, 10) })
+      }
     }).catch(function(err) {
       throw err;
     });
   }
 };
+
+
