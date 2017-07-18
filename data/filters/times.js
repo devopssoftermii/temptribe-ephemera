f17fe6875b70c9f94f130a108e1eb56d5d45f9a8
Ashleigh Crosby <tcrosby@gmail.com>
2017-07-18 13:40:33 +0100

    output string format that mssql can understand

--- data/filters/times.js
+++ data/filters/times.js
@@ -5,6 +5,7 @@ module.exports = function(req, models, output) {
   if (!req.body.f || !Array.isArray(req.body.f.t)) {
     return;
   }
+  var sequelize = req.app.sequelize;
   var [startTime, endTime] = req.body.f.t.map(i => parseFloat(i));
   if (isNaN(startTime) || isNaN(endTime) || endTime < startTime || startTime < 6 || endTime > 30) {
     throw new HttpError(422, {message: 'Invalid time selection (f[t])'});
@@ -15,13 +16,8 @@ module.exports = function(req, models, output) {
       $and: []
     };
   }
-  output.scope.where.$and.push({
-    originalStartTime: {
-      $gte: moment('1900-01-01 00:00:00.000').add(startTime - 0.5, 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS')
-    }
-  }, {
-    originalFinishTime: {
-      $lte: moment('1900-01-01 00:00:00.000').add(endTime + 0.5, 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS')
-    }
-  });
+  output.scope.where.$and.push(
+    sequelize.literal('originalStartTime >= ' + moment('1900-01-01 00:00:00.000').add(startTime - 0.5, 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS')),
+    sequelize.literal('originalFinishTime >= ' + moment('1900-01-01 00:00:00.000').add(endTime + 0.5, 'hours').format('YYYY-MM-DDTHH:mm:ss.SSS'))
+  );
 }
