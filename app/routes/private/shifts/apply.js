64cc401552b4dd3a514aeb85274dcfd31217681a
Ashleigh Crosby <tcrosby@gmail.com>
2017-07-25 14:44:05 +0100

    Apply refactor, fix some bugs

--- app/routes/private/shifts/apply.js
+++ app/routes/private/shifts/apply.js
@@ -18,41 +18,41 @@ module.exports = function(router) {
       if (!shift) {
         throw new ClientError('invalid_shift', { message: 'No such shift' });
       }
-      return Promise.all([shift, shift.getTimesheets()]);
-    }).then(function([shift, timesheets]) {
-      return Promise.all(timesheets.map(function(timesheet) {
-        return timesheet.getUser().then(function(user) {
-          if (timesheet.status === 4 && user.id === req.user.id) {
-            throw new ClientError('already_booked', { message: 'You are already booked on this shift' });
+      return shift.getTimesheets().then(function(timesheets) {
+        return Promise.all(timesheets.map(function(timesheet) {
+          return timesheet.getUser().then(function(user) {
+            if (timesheet.status === 4 && user.id === req.user.id) {
+              throw new ClientError('already_booked', { message: 'You are already booked on this shift' });
+            }
+          }).catch(function(err) {
+            throw err;
+          });
+        }))
+      }).then(function() {
+        return getClashingShifts(models, sequelize, shift, req.user.id).then(function(clashing) {
+          if (clashing && clashing.length) {
+            throw new ClientError('already_booked_other', { message: 'You are already booked on another shift at this time' });
           }
+          return shift;
         }).catch(function(err) {
           throw err;
         });
-      })).then(function() {
-        return shift;
+      }).then(function() {
+        var favourites = new Set(req.user.favouritedBy.map(function(client) {
+          return client.id;
+        }));
+        return bookUserOnShift(
+          models,
+          sequelize,
+          shift,
+          req.user.id,
+          favourites.has(shift.event.client.id)
+        );
       });
-    }).then(function(shift) {
-      return getClashingShifts(models, sequelize, shift, req.user.id).then(function(clashing) {
-        if (clashing && clashing.length) {
-          throw new ClientError('already_booked_other', { message: 'You are already booked on another shift at this time' });
-        }
-        return shift;
-      }).catch(function(err) {
-        throw err;
+    }).then(function(status) {
+      res.json({
+        result: status
       });
-    }).then(function(shift) {
-      var favourites = new Set(req.user.favouritedBy.map(function(client) {
-        return client.id;
-      }));
-      return bookUserOnShift(
-        models,
-        sequelize,
-        shift,
-        req.user.id,
-        favourites.has(shift.event.client.id)
-      );
-    }).then(function(shift) {
-      res.json(shift);
     }).catch(function(err) {
       next(err);
     });
