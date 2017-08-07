0f79f007b3dff3a9f1d2c482ba058682e3d0e22d
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-07 12:21:37 +0100

    implement count param, include count

--- app/routes/private/admin/newusers/list.js
+++ app/routes/private/admin/newusers/list.js
@@ -1,8 +1,8 @@
 module.exports = function(router) {
   router.get('/list', function(req, res, next) {
     var models = req.app.locals.models;
-    var count = req.query.count;
-    if (!count) {
+    var count = parseInt(req.query.count, 10);
+    if (isNaN(count) || !count) {
       count = 1;
     }
     return models.users.scope([
