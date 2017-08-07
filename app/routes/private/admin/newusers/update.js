5da703acf2a23aa41f241a5b2c6c713a6d0b7890
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-07 12:08:33 +0100

    complete domain for invite email

--- app/routes/private/admin/newusers/update.js
+++ app/routes/private/admin/newusers/update.js
@@ -34,7 +34,7 @@ module.exports = function(router) {
         }).then(function(result) {
           return mailer.send('newApplicant', user.email, {
             firstname: user.firstname,
-            url: userHelper.generateURLHash(user, 'inviteToInterview')
+            url: `${process.env.APP_HOST}/recruitment/invite?i=${userHelper.generateURLHash(user, 'inviteToInterview')}`
           });
         }).then(function(result) {
           return {
