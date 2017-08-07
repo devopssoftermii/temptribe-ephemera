affcf5ff3bdba26bd4f1df617c96ca2c9d24dd57
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-07 13:12:04 +0100

    open-ended CORS (for now)

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ if (process.env.NODE_ENV === 'development') {
 // JSON parser
 app.use(require('body-parser').json());
 
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN? process.env.CORS_ORIGIN: '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // App
 var version = require('./package.json').version.split('.');
 var apiPath = `${process.env.API_PREFIX}/v${version[0]}`;
