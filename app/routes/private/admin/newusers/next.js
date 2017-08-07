3c61505be0d1193c6b802b36520a95b85608c7f5
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-07 12:15:07 +0100

    list -> next

--- app/routes/private/admin/newusers/next.js
+++ app/routes/private/admin/newusers/next.js
@@ -1,11 +1,11 @@
 module.exports = function(router) {
-  router.get('/list', function(req, res, next) {
+  router.get('/next', function(req, res, next) {
     var models = req.app.locals.models;
     return models.users.scope([
       'profile',
       'newRegistration'
     ]).findAll({
-      limit: 500
+      limit: 1
     }).then(function(results) {
       res.json(results);
     }).catch(function(err) {
