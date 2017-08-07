07c1c4933f833c8b6a2bd7f7c5429dc1bccb21af
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-07 09:13:50 +0100

    actually return

--- data/models/userTrainingSessionApplications.js
+++ data/models/userTrainingSessionApplications.js
@@ -1,45 +1,45 @@
 /* jshint indent: 1 */
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('userTrainingSessionApplications', {
-		ID: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		UserID: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		TrainingSessionID: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		DateCreated: {
-			type: DataTypes.DATE,
-			allowNull: true,
-			defaultValue: '(getdate())'
-		},
-		Status: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		DateModified: {
-			type: DataTypes.DATE,
-			allowNull: true
-		},
-		ModifiedBy: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		LastAction: {
-			type: DataTypes.STRING,
-			allowNull: true
-		}
-	}, {
-		tableName: 'UserTrainingSessionApplications',
-		timestamps: false,
-		freezeTableName: true
-	});
+  return sequelize.define('userTrainingSessionApplications', {
+    ID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    UserID: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    TrainingSessionID: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    DateCreated: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: '(getdate())'
+    },
+    Status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    DateModified: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    ModifiedBy: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    LastAction: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+    tableName: 'UserTrainingSessionApplications',
+    timestamps: false,
+    freezeTableName: true
+  });
 };
--- data/models/userTrainingSessions.js
+++ data/models/userTrainingSessions.js
@@ -1,25 +1,25 @@
 /* jshint indent: 1 */
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('userTrainingSessions', {
-		id: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		userID: {
-			type: DataTypes.INTEGER,
-			allowNull: true
-		},
-		dateStamp: {
-			type: DataTypes.DATE,
-			allowNull: true,
-			defaultValue: '(getdate())'
-		}
-	}, {
-		tableName: 'userTrainingSessions',
-		timestamps: false,
-		freezeTableName: true
-	});
+  return sequelize.define('userTrainingSessions', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userID: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    dateStamp: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: '(getdate())'
+    }
+  }, {
+    tableName: 'userTrainingSessions',
+    timestamps: false,
+    freezeTableName: true
+  });
 };
