47eb6c86b21ff81d0dbe20517c1a9b90aeac4fd2
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-10 15:31:33 +0100

    unify auth errors

--- middleware/errors.js
+++ middleware/errors.js
@@ -3,14 +3,14 @@ const UnauthorizedError = require('../lib/errors/UnauthorizedError');
 
 module.exports = function(app) {
   app.use(function(err, req, res, next) {
-    var status = err.status || err.status_code || 500;
-    if (process.env.NODE_ENV !== 'development' && status === 500) {
-      err = new ServerError();
-    }
     if (err.name && err.name === 'JsonWebTokenError') {
       err = new UnauthorizedError('token_invalid', {
         message: 'Invalid token'
       });
+    }
+    var status = err.status || err.status_code || 500;
+    if (process.env.NODE_ENV !== 'development' && status === 500) {
+      err = new ServerError();
     }
     res.status(status).jsend(null, err).end();
   });
