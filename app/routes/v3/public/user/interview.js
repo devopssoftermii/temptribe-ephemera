8dfd125967aaf7b9ac699a8a8a904b03746594cc
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-11 16:20:00 +0100

    concrete interview booking

--- app/routes/v3/public/user/interview.js
+++ app/routes/v3/public/user/interview.js
@@ -1,17 +1,22 @@
+const ClientError = require('../../../../../lib/errors/ClientError');
+
+getUserInterview = function(models, userId) {
+  return models.userTrainingSessionApplications.findOne({
+    where: {
+      userID: userId,
+      status: 4
+    },
+    include: [{
+      model: models.trainingSessions,
+      as: 'trainingSession',
+      attributes: ['SessionDate', 'StartTime', 'EndTime']
+    }],
+  })
+}
+
 module.exports = function(router) {
   router.get('/interview', function(req, res, next) {
-    var models = req.app.locals.models;
-    return models.userTrainingSessionApplications.findOne({
-      where: {
-        userID: req.user.id,
-        status: 4
-      },
-      include: [{
-        model: models.trainingSessions,
-        as: 'trainingSession',
-        attributes: ['SessionDate', 'StartTime', 'EndTime']
-      }],
-    }).then(function(result) {
+    return getUserInterview(req.app.locals.models, req.user.id).then(function(result) {
       res.jsend(result || []);
     }).catch(function(err) {
       next(err);
@@ -20,27 +25,31 @@ module.exports = function(router) {
 
   router.post('/interview', function(req, res, next) {
     var models = req.app.locals.models;
-    var trainingSessionID = req.body.trainingSessionID
-    return models.trainingSessions.findById(trainingSessionID).then((trainingSession) => trainingSession.full()).then((full) => {
-      if (full === true) {
-        return {
-          result: 'session_full',
-          trainingSessionID: trainingSessionID
-        };
-      } else {
-        return models.userTrainingSessionApplications.create({
-          UserID: req.user.id,
-          TrainingSessionID: trainingSessionID,
-          Status: 4,
-        }).then((newRecord) => {
-          return {
-            result: 'created',
-            newRecord
-          };
-        }).catch(function(err) {
-          throw err;
+    var trainingSessionID = req.body.trainingSessionID;
+    return getUserInterview(models, req.user.id).then(function(result) {
+      if (result) {
+        throw new ClientError('already_booked', {
+          message: 'You are already booked on an interview'
         });
       }
+      return true;
+    }).then(function() {
+      return models.trainingSessions.findById(trainingSessionID);
+    }).then((trainingSession) => {
+      return trainingSession.full();
+    }).then((full) => {
+      if (full === true) {
+        throw new ClientError('session_full', {
+          message: 'This session is already fully booked'
+        });
+      }
+      return true;
+    }).then(function() {
+      return models.userTrainingSessionApplications.create({
+        UserID: req.user.id,
+        TrainingSessionID: trainingSessionID,
+        Status: 4,
+      });
     }).then(function(result) {
       res.jsend(result);
     }).catch(function(err) {
