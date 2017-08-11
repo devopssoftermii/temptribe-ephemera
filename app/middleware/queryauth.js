eb80ff2e943e2fb5a5b032784bedec2d4141369a
Ashleigh Crosby <tcrosby@gmail.com>
2017-08-11 16:27:36 +0100

    fix queryauth

--- app/middleware/queryauth.js
+++ app/middleware/queryauth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../../lib/errors/UnauthorizedError');
 
-module.exports = function(router) {
+module.exports = function(router, intent) {
   router.use(function(req, res, next) {
     try {
-      req.user = jwt.verify(req.query.jwt, process.env.JWT_SECRET);
+      var payload = jwt.verify(req.query.jwt, process.env.JWT_SECRET);
+      if (!payload || !payload.user || !payload.intent || payload.intent !== intent) {
+        throw new UnauthorizedError('token_invalid', {
+          message: 'Invalid authorization token'
+        });
+      }
+      req.user = payload.user;
       next();
     } catch (err) {
       next(err);
--- app/routes/v3/public/interview/index.js
+++ app/routes/v3/public/interview/index.js
@@ -2,7 +2,7 @@ var router    = require('express').Router(),
     path      = require('path'),
     fs        = require('fs');
 
-require('../../../../middleware/queryauth')(router);
+require('../../../../middleware/queryauth')(router, 'inviteToInterview');
 
 fs.readdirSync(__dirname).forEach(function(filename) {
   var route = path.basename(filename, '.js');
