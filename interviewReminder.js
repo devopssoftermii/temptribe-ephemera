47db8fe722b1b790141f707bedb663055fbd3af9
Ashleigh Crosby <tcrosby@gmail.com>
2017-09-07 17:20:19 +0100

    Select users, send test emails

--- interviewReminder.js
+++ interviewReminder.js
@@ -10,13 +10,13 @@ require('./data')(app);
 
 const { sequelize, models } = app.locals;
 models.trainingSessions.findAll({
-  attributes: ['ID'],
+  attributes: ['ID', 'SessionDate', 'StartTime'],
   include: [{
     model: models.userTrainingSessionApplications,
     include: [{
       model: models.users,
       as: 'user',
-      attributes: ['email'],
+      attributes: ['email', 'firstname'],
       required: true
     }],
     required: true,
@@ -32,12 +32,20 @@ models.trainingSessions.findAll({
     return Promise.all(results.map(function(session) {
       return Promise.all(session.userTrainingSessionApplications.map(function(application) {
         return new Promise(function(resolve, reject) {
-          console.log(application.user.email);
-          resolve();
+          resolve({
+            to: 'ashleigh+' + application.user.firstname + '@temptribe.co.uk',
+            data: {
+              firstname: application.user.firstname,
+              date: session.SessionDate,
+              time: session.StartTime
+            }
+          });
         });
       }));
-    })).then(function() {
-      process.exit();
+    })).then(function(emails) {
+      mailer.sendBatch('interviewReminder', emails).finally(function() {
+        process.exit();
+      });
     });
   }
 });
