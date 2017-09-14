413e5b9c01c7aa1f7e45ade0a00f2b950129369e
Ashleigh Crosby <tcrosby@gmail.com>
2017-09-14 15:54:42 +0100

    repl tweaks

--- console.js
+++ console.js
@@ -2,16 +2,14 @@ var repl = require("repl");
 
 require('dotenv-safe').config();
 
-var app = {locals: 'foo'};
-
+var app = {
+  locals: {}
+};
 require('./data')(app);
 
+// open the repl session
+var replServer = repl.start({
+  prompt: "ephemera > ",
+});
 
-  // open the repl session
-  var replServer = repl.start({
-    prompt: "repl> ",
-  });
-
-replServer.context.db = app.locals
-
-//db.models.trainingSessions.findById(3608).then(x => x.full()).then(x => {console.log(x)})
+replServer.context.db = app.locals;
