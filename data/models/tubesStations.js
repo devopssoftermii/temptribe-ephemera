d0e734aeb1758b25fc133c0571109256ac9ad21b
Tom Nimmo <t.nimmo@gmail.com>
2017-10-09 17:36:44 +0100

    added venue tubes

---
+++ data/models/tubesStations.js
@@ -0,0 +1,41 @@
+/* jshint indent: 1 */
+
+module.exports = function (sequelize, DataTypes) {
+  var tubeStations = sequelize.define('tubeStations', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    links: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    tableName: 'tubeStations',
+    timestamps: false,
+    freezeTableName: true,
+    scopes: {
+      standard: {
+        attributes: ['id', 'description', 'links', 'notes']
+      }
+    }
+  });
+  tubeStations.belongsToMany(models.venues,{
+      through: 'venueTubeLink',
+      foreignKey: 'tubeStationID'
+    });
+  return tubeStations;
+};
