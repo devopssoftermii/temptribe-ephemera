9d70549f810c8d9efd68633bd173accd218e21a4
Ashleigh Crosby <tcrosby@gmail.com>
2017-10-13 10:02:00 +0100

    Save commit

---
+++ app/routes/v3/private/admin/shift/staff/applied.js
@@ -0,0 +1,24 @@
+module.exports = function(router) {
+  router.get('/applied', function(req, res, next) {
+    var { shiftId } = req.params;
+    var models = req.app.locals.models;
+    models.eventShifts.find({
+      where: {
+        id: shiftId
+      },
+      include: [{
+        model: models.userTimesheets.scope('onlyApplied'),
+        as: 'timesheets',
+        include: [{
+          attributes: ['id'],
+          model: models.users,
+          as: 'user',
+        }]
+      }]
+    }).then(function(shift) {
+      res.jsend(shift.timesheets.map((timesheet) => timesheet.user.id));
+    }).catch(function(err) {
+      next(err);
+    });
+  });
+}
