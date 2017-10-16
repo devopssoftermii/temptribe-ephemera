f2592559ce5126b215c9cd0a57f46a9a85ef9ecf
Ashleigh Crosby <tcrosby@gmail.com>
2017-10-16 17:33:53 +0100

    select * from func

--- app/routes/v3/private/admin/shift/staff/available.js
+++ app/routes/v3/private/admin/shift/staff/available.js
@@ -2,7 +2,7 @@ module.exports = function(router) {
   router.get('/available', function(req, res, next) {
     var { shiftId } = req.params;
     var { sequelize } = req.app.locals;
-    return sequelize.query('dbo.udf_availableStaff :shiftId', {
+    return sequelize.query('select * from dbo.udf_availableStaff :shiftId', {
       replacements: {
         shiftId
       }
