6d02cdd3e82184b41e905213695ca74677e9570a
Ashleigh Crosby <tcrosby@gmail.com>
2017-10-16 17:36:03 +0100

    select * from func

--- app/routes/v3/private/admin/shift/staff/available.js
+++ app/routes/v3/private/admin/shift/staff/available.js
@@ -1,8 +1,14 @@
+import ClientError from '../../../../../../../lib/errors/ClientError'
+
 module.exports = function(router) {
   router.get('/available', function(req, res, next) {
     var { shiftId } = req.params;
     var { sequelize } = req.app.locals;
-    return sequelize.query('select * from dbo.udf_availableStaff :shiftId', {
+    shiftId = parseInt(shiftId);
+    if (isNaN(shiftId)) {
+      throw new ClientError('missing_shift', {message: 'Invalid or missing shiftId'});
+    }
+    return sequelize.query('select * from dbo.udf_availableStaff(:shiftId)', {
       replacements: {
         shiftId
       }
