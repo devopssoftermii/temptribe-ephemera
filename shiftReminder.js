7f67c54932dd534274a8fb69059ec7f4b42abd4d
Ashleigh Crosby <tcrosby@gmail.com>
2017-10-25 18:48:30 +0100

    run for 23 and 4

--- shiftReminder.js
+++ shiftReminder.js
@@ -2,6 +2,7 @@
 require('dotenv-safe').config();
 const ClientError = require('./lib/errors/ClientError');
 const mailer = require('./lib/mailer');
+const notifications = require('./lib/notifications');
 const moment = require('moment');
 
 var app = {};
@@ -10,74 +11,112 @@ var app = {};
 require('./data')(app);
 
 const { sequelize, models } = app.locals;
-models.eventShifts.findAll({
-  attributes: ['originalStartTime'],
-  include: [{
-    attributes: ['eventDate'],
-    model: models.events,
-    as: 'event',
+
+function remindShifts(dateStart, dateEnd, timeStart, timeEnd, title, bodyFunc, icon) {
+  return models.eventShifts.findAll({
+    attributes: ['originalStartTime'],
+    include: [{
+      attributes: ['eventDate'],
+      model: models.events,
+      as: 'event',
+      where: {
+        eventDate: {
+          $gte: dateStart,
+          $lt: dateEnd,
+        },
+        status: {
+          $not: 5
+        }
+      },
+      include: [{
+        model: models.venues,
+        as: 'venue',
+        attributes: ['name'],
+        required: true
+      }, {
+        model: models.clients,
+        as: 'client',
+        attributes: ['clientName'],
+        required: true
+      }],
+    }, {
+      model: models.userTimesheets,
+      as: 'timesheets',
+      where: {
+        status: 4
+      },
+      include: [{
+        model: models.users,
+        as: 'user',
+        attributes: ['id', 'firstname', 'email'],
+        required: true
+      }],
+    }],
     where: {
-      eventDate: {
-        $gte: sequelize.fn('dateadd', sequelize.literal('DAY'), 1, sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))),
-        $lt: sequelize.fn('dateadd', sequelize.literal('DAY'), 2, sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))),
+      originalStartTime: {
+        $gte: timeStart,
+        $lt: timeEnd,
       },
       status: {
-        $not: 5
+        $notIn: [0, 2, 7]
       }
-    },
-    include: [{
-      model: models.venues,
-      as: 'venue',
-      attributes: ['name'],
-      required: true
-    }, {
-      model: models.clients,
-      as: 'client',
-      attributes: ['clientName'],
-      required: true
-    }],
-  }, {
-    model: models.userTimesheets,
-    as: 'timesheets',
-    where: {
-      status: 4
-    },
-    include: [{
-      model: models.users,
-      as: 'user',
-      attributes: ['firstname', 'email'],
-      required: true
-    }],
-  }],
-  where: {
-    originalStartTime: {
-      $gte: sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('dateadd', sequelize.literal('HOUR'), -1, sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate')))),
-      $lt: sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate'))),
-    },
-    status: {
-      $notIn: [0, 2, 7]
     }
-  }
-}).then(function(results) {
-  if (results.length) {
-    var emails = [];
-    results.forEach(function(shift) {
-      shift.timesheets.forEach(function(timesheet) {
-        emails.push({
-          to: timesheet.user.email,
-          data: {
-            firstname: timesheet.user.firstname,
-            venue: shift.event.venue.name
-          }
+  }).then(function(results) {
+    if (results.length) {
+      var emails = [];
+      Promise.all(results.map(function(shift) {
+        var userIds = shift.timesheets.map(function(timesheet) {
+          return timesheet.user.id;
         });
+        var body = bodyFunc(shift.originalStartTime, shift.event.client.clientName, shift.event.venue.name);
+        var data = {
+          routeName: 'EventDetails',
+          params: {
+            shiftId: shift.id
+          }
+        };
+        return notifications.send(models, userIds, notificationTitle, body, data, icon).then(function(result) {
+          shift.timesheets.forEach(function(timesheet) {
+            if (result.to.userIds.indexOf(timesheet.user.id) === -1) {
+              emails.push({
+                to: timesheet.user.email,
+                data: {
+                  firstname: timesheet.user.firstname,
+                  venue: shift.event.venue.name
+                }
+              });
+            }
+          });
+        });
+      })).then(function() {
+        mailer.sendBatch('shiftReminder', emails);
       });
-    })
-    mailer.sendBatch('shiftReminder', emails).then(function() {
-      process.exit();
-    }).catch(function() {
-      process.exit();
-    });
-  } else {
-    process.exit();
-  }
+    }
+  });
+}
+
+Promise.all([remindShifts(
+  sequelize.fn('dateadd', sequelize.literal('DAY'), 1, sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))),
+  sequelize.fn('dateadd', sequelize.literal('DAY'), 2, sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))),
+  sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('dateadd', sequelize.literal('HOUR'), -1, sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate')))),
+  sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate'))),
+  'Shift Reminder 🔔',
+  function(startTime, client, venue) {
+    return `You’ve got a shift tomorrow at ${startTime} for ${client} at ${venue}! Make sure you read the shift notes carefully to help you to get ready!`
+  },
+  'ic_shift_reminder',
+), remindShifts(
+  sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate')),
+  sequelize.fn('dateadd', sequelize.literal('DAY'), 1, sequelize.fn('convert', sequelize.literal('DATE'), sequelize.fn('getdate'))),
+  sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('dateadd', sequelize.literal('HOUR'), 4, sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate')))),
+  sequelize.fn('convert', sequelize.literal('DATETIMEOFFSET'), sequelize.fn('dateadd', sequelize.literal('HOUR'), 5, sequelize.fn('convert', sequelize.literal('TIME'), sequelize.fn('getdate')))),
+  'Time to Get Ready ⏱',
+  function(startTime, client, venue) {
+    return `Your shift today starts at ${startTime}! Make sure you know when to arrive, what to wear and where to meet your tribe!`
+  },
+  'ic_shift_reminder',
+)]).then(function() {
+  process.exit();
+}).catch(function() {
+  process.exit();
 });
