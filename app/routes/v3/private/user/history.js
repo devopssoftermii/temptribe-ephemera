7d5132595728f8f260eff65441c75553756df36c
Ashleigh Crosby <tcrosby@gmail.com>
2017-11-25 18:50:03 +0000

    select work history from new sql function

---
+++ app/routes/v3/private/user/history.js
@@ -0,0 +1,41 @@
+const HISTORY_STATUSES = new Map([
+  ['incomplete', 0],
+  ['unconfirmed', 1],
+  ['complete', 2],
+]);
+
+module.exports = function(router) {
+  router.get('/history', function(req, res, next) {
+    var sequelize = req.app.locals.sequelize;
+    var { status, page, size } = req.query;
+    if (!status || !HISTORY_STATUSES.has(status)) {
+      status = 'complete';
+    }
+    if ('undefined' === typeof(page)) {
+      page = 1;
+    }
+    if (!size) {
+      size = 10;
+    }
+    return sequelize.query(`select
+      rownum as num, paid, timesheetStatus,
+      staffStartTime, staffEndTime, staffBreaks, staffWorked,
+      clientStartTime, clientEndTime, clientBreaks, clientWorked,
+      originalStartTime, originalEndTime, originalBreaks,
+      hourlyRate, date, eventTitle, eventSubtitle, venueName
+    from dbo.udf_userWorkHistory(:userId, :status, :page, :size)`, {
+      replacements: {
+        userId: req.user.id,
+        status: HISTORY_STATUSES.get(status),
+        page,
+        size
+      }
+    }).then(function(result) {
+      res.jsend(result[0].map(function(user) {
+        return user.userid
+      }));
+    }).catch(function(err) {
+      next(err);
+    });
+  });
+}
